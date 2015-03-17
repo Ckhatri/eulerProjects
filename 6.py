2efc316eb4ee3e389d1726011d56de9d89ff5807
ckhatri <chirag.k.khatri@gmail.com>
2015-03-16 22:17:05 -0500

    Updated to be made more efficient by using the Triangular Number formula.

--- 6.py
+++ 6.py
@@ -4,11 +4,9 @@ def sumOfSquares(numSquares):
 		theSum += (n * n)
 	return theSum
 
+#uses the summation formula, then squares it. Changes it from O(N) (brute force) to O(1)
 def squareOfSum(numVals):
-	theSum = 0
-	for n in range(numVals + 1):
-		theSum += n
-		
+	theSum = (numVals * (numVals + 1)) / 2
 	theSum = theSum * theSum
 	return theSum
 
