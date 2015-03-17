c675b8b8517ac887d690e8c5734c7bda50fe4d58
ckhatri <chirag.k.khatri@gmail.com>
2015-03-17 16:23:00 -0500

    Finds the 10001st prime number

---
+++ 7.py
@@ -0,0 +1,6 @@
+import eratosthenes
+
+#this program uses my eratosthenes sieve written in python. This sieve is available on my github as well.
+
+theList = eratosthenes.sieve(1000000)
+print theList[10001]
