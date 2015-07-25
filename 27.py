561c81e7d6ac0974fe9b7de60f6490b938c192fb
ckhatri <chirag.k.khatri@gmail.com>
2015-07-24 19:29:01 -0500

    B must be a prime number to start, which greatly reduces the amount of b's looped througH

--- 27.py
+++ 27.py
@@ -1,16 +1,11 @@
-#first let us generate a list of primes, so that we can then check.
-#brute force, slow but within a minute
+#first let us generate a list of primes, so that we can then check, if the numbers generated through the
+#quadratic equation are actually prime
+
+
+#n must start at 0 to be consecutive, so that means n^2 + an + b is just b which means b must be prime
+#b must be some prime number below 1000, taking that into account you reduce the number of b's looped through
 
 import eratosthenes
-
-# def isPrime(number):
-# 	listOfPrimes = eratosthenes.sieveOfEratosthenes(10000)
-# 	pos = 0
-# 	while (number >= listOfPrimes[pos]):
-# 		if (listOfPrimes[pos] == number):
-# 			return True
-# 		pos += 1
-# 	return False
 
 
 def calculate(a, b, n):
@@ -21,8 +16,9 @@ def main():
 	bestB = 0
 	bestN = 0
 	listOfPrimes = eratosthenes.sieveOfEratosthenes(10000)
+	listOfBPrimes = eratosthenes.sieveOfEratosthenes(1000)
 	for a in xrange(-1000, 1000):
-		for b in xrange(-1000, 1000):
+		for b in listOfBPrimes:
 			n = 0
 			while (calculate(a, b, n) in listOfPrimes):
 				n+= 1
