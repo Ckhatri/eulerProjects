ed72c3f4429d1cf634887c4a61170acb956dc3a2
ckhatri <chirag.k.khatri@gmail.com>
2016-04-10 15:56:59 -0500

    updated problem ten, no need for using range

--- 10.py
+++ 10.py
@@ -3,8 +3,8 @@ import eratosthenes
 #this program uses my eratosthenes sieve written in python. This sieve is available on my github as well.
 #the sieve gives me a list of all primes below the parameter passed in.
 
-theList = eratosthenes.sieve(2000000)
+theList = eratosthenes.sieveOfEratosthenes(2000000)
 theSum = 0
-for n in range(len(theList)):
-	theSum += theList[n]
+for num in theList:
+	theSum += num
 print theSum
